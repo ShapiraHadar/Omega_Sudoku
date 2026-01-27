c4a7cd58e069abb4d6101ef9f57a7d3956aba728
Hadar <shapira1hadar@gmail.com>
2026-01-27 13:30:18 +0200

    progress on solver - initiate bitmaps and candidate logic

--- OmegaSudoku/Program.cs
+++ OmegaSudoku/Program.cs
@@ -7,33 +7,33 @@ using System.Threading.Tasks;
 
 namespace OmegaSudoku
 {
-    class SudokuBoard
+    public class SudokuBoard
     {
-        private int squareSize; //3 by default
-        private int boardSize; //9 by default
-        private int[,] board; // when getting input, if taking characters, convert to ints.
+        private int _squareSize; //3 by default
+        private int _boardSize; //9 by default
+        private int[,] _board; // when getting input, if taking characters, convert to ints.
 
-        public int GetSquareSize() {  return squareSize; }
-        public int GetBoardSize() { return boardSize; }
-        public int[,] GetBoard() { return board; }
+        public int GetSquareSize() {  return _squareSize; }
+        public int GetBoardSize() { return _boardSize; }
+        public int[,] GetBoard() { return _board; }
         public SudokuBoard() : this(3) { } // default ctor - 9*9 empty board
         public SudokuBoard(int squareSize) // single int ctor - empty (squareSize^2)*(squareSize^2) board
         {
-            this.squareSize = squareSize;
-            this.boardSize = (int)Math.Pow(squareSize, 2);
-            this.board = new int[boardSize,boardSize];
-            for (int i = 0; i < this.boardSize; i++)
-                for (int j = 0; j < this.boardSize; j++)
-                    this.board[i, j] = 0;
+            this._squareSize = squareSize;
+            this._boardSize = (int)Math.Pow(squareSize, 2);
+            this._board = new int[this._boardSize,this._boardSize];
+            for (int i = 0; i < this._boardSize; i++)
+                for (int j = 0; j < this._boardSize; j++)
+                    this._board[i, j] = 0;
         }
         public SudokuBoard(SudokuBoard board) // copy ctor
         {
-            this.squareSize=board.GetSquareSize();
-            this.boardSize=board.GetBoardSize();
-            this.board = new int[this.boardSize, this.boardSize];
-            for (int i = 0; i < this.boardSize; i++)
-                for (int j = 0; j < this.boardSize; j++)
-                    this.board[i,j] = board.GetBoard()[i,j];
+            this._squareSize=board.GetSquareSize();
+            this._boardSize=board.GetBoardSize();
+            this._board = new int[this._boardSize, this._boardSize];
+            for (int i = 0; i < this._boardSize; i++)
+                for (int j = 0; j < this._boardSize; j++)
+                    this._board[i,j] = board.GetBoard()[i,j];
         }
         public SudokuBoard(int[,] board) // filled board ctor
         {
@@ -42,12 +42,12 @@ namespace OmegaSudoku
                 throw new Exception("Board size must be a perfect square");
             if (board.GetLength(0) != board.GetLength(1))
                 throw new Exception("Board must be a square");
-            this.squareSize = (int)size;
-            this.boardSize = board.GetLength(0);
-            this.board = new int[this.boardSize, this.boardSize];
-            for (int i = 0;i < this.boardSize;i++)
-                for (int j = 0;j < this.boardSize;j++)
-                    this.board[i,j] = board[i,j];
+            this._squareSize = (int)size;
+            this._boardSize = board.GetLength(0);
+            this._board = new int[this._boardSize, this._boardSize];
+            for (int i = 0;i < this._boardSize;i++)
+                for (int j = 0;j < this._boardSize;j++)
+                    this._board[i,j] = board[i,j];
         }
         public SudokuBoard(string s) // string ctor
         {
@@ -55,48 +55,48 @@ namespace OmegaSudoku
             double squareSize=Math.Sqrt(boardSize);
             if (squareSize != (int)squareSize)
                 throw new Exception("Board size must be a perfect square");
-            this.boardSize = (int)boardSize;
-            this.squareSize= (int)squareSize;
-            this.board = new int[this.boardSize, this.boardSize];
-            for (int i = 0; i < this.boardSize; i++)
-                for (int j = 0; j < this.boardSize; j++)
-                    this.board[i, j] = s[i * this.boardSize + j] - '0'; // only for 9*9 currently, easy to modify
+            this._boardSize = (int)boardSize;
+            this._squareSize= (int)squareSize;
+            this._board = new int[this._boardSize, this._boardSize];
+            for (int i = 0; i < this._boardSize; i++)
+                for (int j = 0; j < this._boardSize; j++)
+                    this._board[i, j] = s[i * this._boardSize + j] - '0'; // only for 9*9 currently, easy to modify
         }
         public bool CheckComplete()
         {
             if(!this.CheckFilled())
                 return false;
-            for (int i = 0; i < this.boardSize; i++)
+            for (int i = 0; i < this._boardSize; i++)
                 if(!this.CheckCol(i) || !this.CheckRow(i))
                     return false;
-            for(int i=0;i<this.squareSize;i++)
-                for(int j=0;j<this.squareSize;j++)
+            for(int i=0;i<this._squareSize;i++)
+                for(int j=0;j<this._squareSize;j++)
                     if(!CheckSquare(i,j))
                         return false;
             return true;
         }
         public bool CheckFilled()
         {
-            for (int i = 0; i < this.boardSize; i++)
-                for (int j = 0; j < this.boardSize; j++)
-                    if (this.board[i, j] == 0) return false;
+            for (int i = 0; i < this._boardSize; i++)
+                for (int j = 0; j < this._boardSize; j++)
+                    if (this._board[i, j] == 0) return false;
             return true;
         }
         public bool CheckRow(int row)
         {
             int temp = 0;
-            int[] nums=new int[this.boardSize];
-            if(row<0  || row>=this.boardSize) return false;
-            for(int i = 0; i < this.boardSize; i++)
+            int[] nums=new int[this._boardSize];
+            if(row<0  || row>=this._boardSize) return false;
+            for(int i = 0; i < this._boardSize; i++)
             {
-                if (this.board[row, i] != 0)
+                if (this._board[row, i] != 0)
                 {
                     for (int j = 0; j < temp; j++)
                     {
-                        if(nums[j]==this.board[row,i])
+                        if(nums[j]==this._board[row,i])
                             return false;
                     }
-                    nums[temp] = this.board[row, i];
+                    nums[temp] = this._board[row, i];
                     temp++;
                 }
             }
@@ -105,18 +105,18 @@ namespace OmegaSudoku
         public bool CheckCol(int col)
         {
             int temp = 0;
-            int[] nums = new int[this.boardSize];
-            if (col < 0 || col >= this.boardSize) return false;
-            for (int i = 0; i < this.boardSize; i++)
+            int[] nums = new int[this._boardSize];
+            if (col < 0 || col >= this._boardSize) return false;
+            for (int i = 0; i < this._boardSize; i++)
             {
-                if (this.board[i, col] != 0)
+                if (this._board[i, col] != 0)
                 {
                     for (int j = 0; j < temp; j++)
                     {
-                        if (nums[j] == this.board[i,col])
+                        if (nums[j] == this._board[i,col])
                             return false;
                     }
-                    nums[temp] = this.board[i,col];
+                    nums[temp] = this._board[i,col];
                     temp++;
                 }
             }
@@ -125,18 +125,18 @@ namespace OmegaSudoku
         public bool CheckSquare(int squareRow, int squareCol)
         {
             int temp = 0;
-            int[] nums = new int[this.boardSize];
-            if (squareRow < 0 || squareCol < 0 || squareRow >= this.squareSize || squareCol >= this.squareSize)
+            int[] nums = new int[this._boardSize];
+            if (squareRow < 0 || squareCol < 0 || squareRow >= this._squareSize || squareCol >= this._squareSize)
                 return false;
-            for(int i = this.squareSize * squareRow;i < this.squareSize * (squareRow + 1); i++)
-                for (int j = this.squareSize * squareCol; j < this.squareSize * (squareCol + 1); j++)
+            for(int i = this._squareSize * squareRow;i < this._squareSize * (squareRow + 1); i++)
+                for (int j = this._squareSize * squareCol; j < this._squareSize * (squareCol + 1); j++)
                 {
                     for (int k = 0; k < temp; k++)
                     {
-                        if (nums[k] == this.board[i,j])
+                        if (nums[k] == this._board[i,j])
                             return false;
                     }
-                    nums[temp] = this.board[i,j];
+                    nums[temp] = this._board[i,j];
                     temp++;
                 }
             return true;
@@ -145,25 +145,25 @@ namespace OmegaSudoku
         {
             string result = "";
             string seperator = "";
-            for (int i = 0; i < squareSize; i++)
+            for (int i = 0; i < this._squareSize; i++)
             {
                 seperator += "+";
-                for (int j = 0; j < squareSize * 2 + 1; j++) seperator += "-";
+                for (int j = 0; j < this._squareSize * 2 + 1; j++) seperator += "-";
             }
             seperator += "+\n";
 
-            for (int i = 0; i < squareSize; i++)
+            for (int i = 0; i < this._squareSize; i++)
             {
                 result += seperator;
-                for (int j = 0; j < squareSize; j++)
+                for (int j = 0; j < this._squareSize; j++)
                 {
                     string line = "";
-                    for (int k = 0; k < squareSize; k++)
+                    for (int k = 0; k < this._squareSize; k++)
                     {
                         line += "| ";
-                        for (int t = 0; t < squareSize; t++)
+                        for (int t = 0; t < this._squareSize; t++)
                         {
-                            line += board[i * squareSize + j, k * squareSize + t];
+                            line += this._board[i * this._squareSize + j, k * this._squareSize + t];
                             line += " ";
                         }
                     }
---
+++ OmegaSudoku/SudokuSolver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmegaSudoku
+{
+    public class SudokuSolver
+    {
+        private SudokuBoard _board;
+
+        public void SetBoard(SudokuBoard board)
+        {
+            this._board = board;
+        }
+        public SudokuBoard GetBoard()
+        {
+            return this._board;
+        }
+        public SudokuSolver(SudokuBoard board)
+        {
+            this._board = board;
+        }
+        
+        public SudokuBoard Solve()
+        {
+            
+        }
+
+    }
+}
